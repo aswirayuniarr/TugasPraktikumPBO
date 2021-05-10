ae5ceadcc1f6adc701b7d3a2f922ba675b8648ee
aswirayuniarr <81635511+aswirayuniarr@users.noreply.github.com>
2021-05-10 23:01:38 +0800

    Add files via upload

---
+++ Tugas Praktikum PBO/tugasPert5/src/tugaspert5/TugasPert5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspert5;
+
+/**
+ *
+ * @author Aswira Yuniar
+ */
+public class TugasPert5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         View view = new View();
+        // TODO code application logic here
+    }
+    
+}
